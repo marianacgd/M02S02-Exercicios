f186f9630b51d2ed40fbe8001d5f806ee9c07325
Mariana Daruix <mariana.daruix@gmail.com>
2023-04-12 20:08:07 -0300

    M02S02-Exercicios

--- Ex01Soma/Program.cs
+++ Ex01Soma/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num1, num2, soma;
+
+        Console.Write("Digite um número inteiro: ");
+        num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite outro número inteiro: ");
+        num2 = int.Parse(Console.ReadLine());
+        
+        soma = num1 + num2;
+
+        Console.WriteLine("A soma das números " + num1 + " + " + num2 + " = " + soma);
+
+    }    
+}
