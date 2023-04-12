ad1a621ee3bf155a2d6f1574939843a1cb4f4388
Mariana Daruix <mariana.daruix@gmail.com>
2023-04-12 20:17:05 -0300

    M02S02EX02

--- Ex02ParOuImpar/Program.cs
+++ Ex02ParOuImpar/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numero;
+
+        Console.Write("Digite um número inteiro: ");
+        numero = int.Parse(Console.ReadLine());
+        
+        if (numero % 2 == 0 ) {
+            Console.WriteLine("O número " + numero + " é Par!");
+        } else {
+            Console.WriteLine("O número " + numero + " é Impar!");
+        }
+
+    }    
+}
