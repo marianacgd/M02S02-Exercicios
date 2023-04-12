7ddda98e2307f3937099b60f181a13ad1c9a098c
Mariana Daruix <mariana.daruix@gmail.com>
2023-04-12 20:33:47 -0300

    M02S02EX03

--- Ex03NomeIdade/Program.cs
+++ Ex03NomeIdade/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string nome;
+        int idade;
+
+        Console.Write("Digite o seu nome: ");
+        nome = Console.ReadLine();
+
+        Console.Write("Digite sua idade: ");
+        idade = int.Parse(Console.ReadLine());
+
+         Console.WriteLine("Olá Sr(a) " + nome + " !");
+        
+        if (idade < 18) {
+            Console.WriteLine("Você é Menor de idade!");
+        } else if(idade >= 18 && idade < 60) {
+            Console.WriteLine("Você é Maior de idade!");
+        } else if(idade >= 60){
+            Console.WriteLine("Você é Idoso!");
+        } 
+        
+    }    
+}
