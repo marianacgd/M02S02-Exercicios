7aafe80a4573799f9f476e06ddc78a2968ba70dd
Mariana Daruix <mariana.daruix@gmail.com>
2023-04-12 20:42:57 -0300

    M02S02EX04

--- Ex04Veiculos/Program.cs
+++ Ex04Veiculos/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string marca, modelo;
+        double km;
+
+        Console.Write("Digite a marca do veículo: ");
+        marca = Console.ReadLine();
+
+        Console.Write("Digite o modelo do veículo: ");
+        modelo = Console.ReadLine();
+
+        Console.Write("Digite os Km rodados : ");
+        km = double.Parse(Console.ReadLine());
+
+         Console.WriteLine("Marca: " + marca + " Modelo: " + modelo);
+        
+        if (km > 10000) {
+            Console.WriteLine("Km Rodado: " + km + " - Está na hora de fazer a Revisão no veículo!");
+        } else {
+            Console.WriteLine("Km Rodado: " + km + " - Está tudo em ordem!" );
+        } 
+        
+    }    
+}
