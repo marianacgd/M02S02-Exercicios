70f2fd7cf809bd35b24cdf4275fe665b51600ada
Mariana Daruix <mariana.daruix@gmail.com>
2023-04-12 21:13:32 -0300

    M02S02EX05

--- Ex05OrdenandoNumeros/Program.cs
+++ Ex05OrdenandoNumeros/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numeros = new int[4];
+
+        for (int i = 0; i < 4; i++){
+            Console.Write($"Digite o {i+1}º número: ");
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array.Sort(numeros);
+
+        for (int i = 0; i < numeros.Length; i++) {
+            Console.WriteLine(numeros[i]);
+        }
+        
+    }    
+}
