e9b377159c542653667d50a031da2b4e795fab84
Mariana Daruix <mariana.daruix@gmail.com>
2023-04-13 20:44:18 -0300

    M02S02EX07

--- Ex07NumerosPares/Program.cs
+++ Ex07NumerosPares/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+      int numero;
+
+      do {
+        Console.Write("Digite um número inteiro positivo: ");
+        numero = int.Parse(Console.ReadLine());
+
+        if (numero <= 0) {
+            Console.WriteLine("Erro! o número deve ser positivo e diferente de zero.");
+        }
+      } while (numero <= 0);
+
+      Console.WriteLine($"Números pares até {numero}:");
+
+      for (int i = 0; i <= numero; i += 2){
+        Console.Write(i + " ");
+      }
+
+      Console.WriteLine();
+
+    }    
+}
