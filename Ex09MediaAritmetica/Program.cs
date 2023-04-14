e30c1fc059a7873f1203ef0d1997f7872a6435f7
Mariana Daruix <mariana.daruix@gmail.com>
2023-04-13 21:13:58 -0300

    M02S02EX09

--- Ex09MediaAritmetica/Program.cs
+++ Ex09MediaAritmetica/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+      Console.WriteLine("Digite a quantidade de números que você deseja calcular a média: ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        double[] numeros = new double[quantidade];
+
+        for (int i = 0; i < quantidade; i++) {
+            Console.WriteLine($"Digite o {i+1}º número: ");
+            numeros[i] = double.Parse(Console.ReadLine());
+        }
+
+        double soma = 0;
+        foreach (double numero in numeros) {
+            soma += numero;
+        }
+
+        double media = soma / quantidade;
+        Console.WriteLine($"A média aritmética dos números digitados é: {media}");
+
+    }    
+}
