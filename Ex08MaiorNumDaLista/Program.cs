9ffeddfb99042b64355d05b6f1d124b115825151
Mariana Daruix <mariana.daruix@gmail.com>
2023-04-13 21:02:30 -0300

    M02S02EX08

--- Ex08MaiorNumDaLista/Program.cs
+++ Ex08MaiorNumDaLista/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+      int[] numeros = new int[5];
+
+      for (int i = 0; i < 5; i++){
+        Console.Write($"Digite o {i+1}º número inteiro: ");
+        numeros[i] = int.Parse(Console.ReadLine());
+      }
+
+      int numMaior = numeros[0];
+      for (int i = 1; i < 5; i++) {
+        if (numeros[i] > numMaior){
+            numMaior = numeros[i];
+        }
+      }
+
+      Console.WriteLine($"O maior número digitado foi {numMaior}.");
+
+    }    
+}
