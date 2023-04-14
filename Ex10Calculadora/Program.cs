9f1c18f6a08cefcf58caf6b2ca49ac536b0294d8
Mariana Daruix <mariana.daruix@gmail.com>
2023-04-13 21:40:37 -0300

    M02S02EX10

--- Ex10Calculadora/Program.cs
+++ Ex10Calculadora/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double numero1, numero2;
+        char operacao;
+
+        Console.WriteLine("Olá! Seja bem-vinda(o) a Calculadora!\n");
+
+        do
+        {
+            Console.WriteLine("Agora, escolha uma operação: \n");
+
+            Console.WriteLine("+ : SOMAR");
+            Console.WriteLine("- : SUBTRAIR");
+            Console.WriteLine("* : MULTIPLICAR");
+            Console.WriteLine("/ : DIVIDIR");
+            Console.WriteLine("? : PARA SAIR");
+
+            //Entradas
+            Console.Write("\nDigite a operação escolhida: ");
+            operacao = char.Parse(Console.ReadLine());
+
+            if (operacao != '?')
+            {
+                Console.WriteLine("Digite o primeiro número: ");
+                numero1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o segundo número: ");
+                numero2 = int.Parse(Console.ReadLine());
+
+                switch (operacao)
+                {
+                    case '+':
+                        Console.WriteLine("Soma: " + (numero1 + numero2));
+                        break;
+                    case '-':
+                        Console.WriteLine("Subtração: " + (numero1 - numero2));
+                        break;
+                    case '*':
+                        Console.WriteLine("Multiplicação: " + (numero1 * numero2));
+                        break;
+                    case '/':
+                        Console.WriteLine("Divisão: " + (numero1 / numero2));
+                        break;
+                    default:
+                        Console.WriteLine("ERRO! ");
+                        break;
+                }
+
+            }
+
+        } while (operacao != '?');
+
+        Console.WriteLine("\nObrigado por usar a calculadora!");
+     
+
+    }    
+}
